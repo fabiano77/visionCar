92e85cc136cdb1af79e749123be1f95d14846204
fabiano77 <59503581+fabiano77@users.noreply.github.com>
2020-04-13 11:11:07 +0900

    Update CustomPicar.h
    set_val 인자값 설명 추가

--- CustomPicar.h
+++ CustomPicar.h
@@ -11,8 +11,17 @@ class Servo
 public:
 	Servo(PCA9685 pca_, int motorPin, int timeTerm = 2000);		//초기 설정은 만질 일 없음.
 	void setValue(uint16_t set_val);	//서보의 각도를 설정하는 함수.인자로 넘겨주는 값 190~530 가용범위이고, 360이 중심이다. 
+						//서보가동 pulse width : 0.75 ~1.5~ 2.25ms
+						//set_val 인자로 환산시 : 185 ~(370)~ 550
+						//0번 조향바퀴 270좌 ~ 370중심 ~ 470우
+						//1번 cam좌우 530(550)좌 ~ 360중심 ~ 190(150)우 
+									// 괄호는 한계
+						//2번 cam상하 650상 ~ 350중심 ~ 300하
+						//4번 우측바퀴 1000~4000
+						//5번 좌측바퀴 1000~4000
+	
 	void setRatio(double ratio);		//서보의 각도를 %비율로 설정하는 함수. 가용범위 0~100 이며 50% 이 중심.
-	void resetCenter();			//서보의 각도를 중심으로 초기화하는 함수. tilt 서보는 50%이 중심이 아님. 약 30%정도
+	void resetCenter();			//서보의 각도를 중심으로 초기화하는 함수. tilt 서보는 50%이 중심이 아님. 약 20%정도
 	void operator++(int);			//서보의 각도를 10% 증가
 	void operator--(int);			//서보의 각도를 10% 감소
 private:
