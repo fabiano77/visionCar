5034eaef459a8d4ac02cb9fba99c4d3c31e62671
Kim Daehee <fabiano7@naver.com>
2020-05-13 17:03:57 +0900

    new

--- new_main.cpp
+++ new_main.cpp
@@ -112,8 +112,8 @@ int main()
 		Driving_DH DH(true, 1.00);	//printFlag, sLevel
 		//DH.mappingSetSection(0, 0.10, 0.40, 0.73, 0.79, 1.00);
 		//DH.mappingSetValue(8.0, 8.00, 15.0, 22.0, 50.0, 50.0);	//코너구간 조향수준 맵핑값 세팅
-		DH.mappingSetSection(0, 0.10, 0.40, 0.75, 0.79, 1.00);
-		DH.mappingSetValue(8.0, 8.00, 15.0, 20.0, 50.0, 50.0);	//코너구간 조향수준 맵핑값 세팅
+		DH.mappingSetSection(0, 0.10, 0.40, 0.77, 0.81, 1.00);
+		DH.mappingSetValue(8.0, 8.00, 10.0, 20.0, 50.0, 50.0);	//코너구간 조향수준 맵핑값 세팅
 		double steerVal(50.0);	//초기 각도(50이 중심)
 		double speedVal(40.0);	//초기 속도(0~100)
 		bool cornerFlag(false);
