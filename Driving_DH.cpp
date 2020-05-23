1f204053b4957ff68c0cb31f9476300cf4fa7231
Kim Daehee <fabiano7@naver.com>
2020-05-23 12:02:30 +0900

    디버그

--- Driving_DH.cpp
+++ Driving_DH.cpp
@@ -2,7 +2,7 @@
 
 const bool PRINT(true);				// 영상에 출력 표시 on&off
 const bool PRINT_RESULT(true);			// 결과직선 표시 on&off
-const bool IMAGE_DEBUG(false);
+const bool IMAGE_DEBUG(true);
 double STRAIGHT_LEVEL(1.00);	// 직선 구간에서의 가중치 커지면 각도 쎄게틈
 int straightSteer = 7;		// 44~56
 int straightLower = 50 - straightSteer;
