eba9c9fa7480ea13e8520a84e6fb71909d5c49b4
Kim Daehee <fabiano7@naver.com>
2020-05-25 23:47:08 +0900

    new

--- main2.cpp
+++ main2.cpp
@@ -192,15 +192,15 @@ int main()
 				if (!cornerFlag && (steerVal == 90 || steerVal == 10))
 				{
 					cornerFlag = true;
-					DH.mappingSetSection(0, 0.15, 0.35, 0.50, 0.54, 0.58);
-					DH.mappingSetValue(7.0, 10.0, 15.0, 20.0, 35.0, 40.0);
+					DH.mappingSetSection(0, 0.15, 0.35, 0.45, 0.52, 0.56);
+					DH.mappingSetValue(7.0, 10.0, 15.0, 25.0, 35.0, 40.0);
 					cout << "cornerFlag ON" << '\n';
 				}
 				//else if (cornerFlag && steerVal >= 43 && steerVal <= 57)
 				else if (cornerFlag && detectedLineCnt == 2)
 				{
 					cornerFlag = false;
-					DH.mappingSetSection(0, 0.15, 0.35, 0.50, 0.54, 0.58);
+					DH.mappingSetSection(0, 0.15, 0.35, 0.45, 0.52, 0.56);
 					DH.mappingSetValue(7.0, 7.00, 0.00, -4.0, 0.00, 40.0);
 					cout << "cornerFlag OFF" << '\n';
 				}
@@ -241,7 +241,7 @@ int main()
 					{
 						cornerFlag = true;
 						DH.mappingSetSection(0, 0.15, 0.35, 0.50, 0.55, 0.60);
-						DH.mappingSetValue(7.0, 7.00, 10.0, 20.0, 35.0, 40.0);
+						DH.mappingSetValue(7.0, 7.00, 12.0, 25.0, 35.0, 40.0);
 						cout << "cornerFlag ON" << '\n';
 					}
 					//else if (cornerFlag && steerVal >= 43 && steerVal <= 57)
