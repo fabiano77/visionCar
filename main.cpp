7cdc0260f0cf798356c16c59a3d71a557e6f1bfb
석준 이 <ykykyk112@naver.com>
2020-05-26 17:17:58 +0900

    주차 실제 테스트용.ver.2.1

--- main.cpp
+++ main.cpp
@@ -368,7 +368,7 @@ int main()
 						DCmotor.stop();
 						waitKey(500);
 						DCmotor.backward();
-						waitKey(1000);
+						waitKey(700);
 						DCmotor.stop();
 						waitKey(500);
 						steering.setRatio(90); // 바퀴를 오른쪽으로 돌린 후 후진
