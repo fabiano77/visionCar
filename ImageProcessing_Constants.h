4550e70cd76d191f0447b2e2f3f39908b7a47eb8
Kim Daehee <fabiano7@naver.com>
2020-06-04 18:55:03 +0900

    상수

--- ImageProcessing_Constants.h
+++ ImageProcessing_Constants.h
@@ -1,6 +1,6 @@
 #ifndef IMAGEPROCESSING_CONSTANT_H
 #define IMAGEPROCESSING_CONSTANT_H
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>//
 using namespace cv;
 
 const int MAX_SAVINGANGLE=5;
